f240bd37ba03c643b55ccfb7c0d73b7c0174464f
Martin Hellmich <mhellmic@gmail.com>
2013-05-02 11:59:39 +0200

    made the get_key methods more generic

--- lib/manipulate_bamboo_json.py
+++ lib/manipulate_bamboo_json.py
@@ -1,5 +1,17 @@
-def get_keys(bamboo_entity, entity_type):
-  entity_list = bamboo_entity[entity_type+'s'][entity_type]
-  return map(lambda d: d['key'], entity_list)
+def _get_value_from_bamboo_dict(bamboo_dict, dict_type, type_value):
+  entity_list = bamboo_dict[dict_type+'s'][dict_type]
+  return map(lambda d: d[type_value], entity_list)
 
+def get_keys(bamboo_dict, dict_type):
+  return _get_value_from_bamboo_dict(bamboo_dict,
+                                     dict_type,
+                                     'key')
+def get_project_keys(bamboo_dict):
+  return _get_value_from_bamboo_dict(bamboo_dict,
+                                     'project',
+                                     'key')
 
+def get_plan_keys(bamboo_dict):
+  return _get_value_from_bamboo_dict(bamboo_dict,
+                                     'plan',
+                                     'key')
