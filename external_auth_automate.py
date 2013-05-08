8fb68c150d8ae35e64d26becbbed98ada5fdd6ed
Martin Hellmich <mhellmic@gmail.com>
2013-05-08 15:25:41 +0200

    removed example files which tend to be outdated. the readme file should include all the main use cases

--- external_auth_automate.py
+++
@@ -1,7 +0,0 @@
-from lib.requests import *
-from lib.bamboo_commands import *
-
-conn = external_authenticate('http://localhost:6990', 'cookies.txt')
-res = add_mod_plan_variable(conn, 'LCGDM-PUPPET', 'automate', 'them')
-print res
-
