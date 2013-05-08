589650274f6a544ff7b733eddeadae5488917243
Martin Hellmich <mhellmic@gmail.com>
2013-05-08 15:24:13 +0200

    add single lib file to include

---
+++ lib/bamboo_automate.py
@@ -0,0 +1,7 @@
+from bamboo_commands import *
+from high_level_functions import *
+from manipulate_bamboo_json import *
+
+from bamboo.plan_branches import *
+
+logging.basicConfig(level=logging.DEBUG)
