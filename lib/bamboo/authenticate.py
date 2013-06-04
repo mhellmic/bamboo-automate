1601e30f8b216037380f81a7383e866b9bf25b3d
Martin Hellmich <mhellmic@gmail.com>
2013-06-04 08:38:23 +0200

    removed publicsuffixlist dependency

--- lib/bamboo/authenticate.py
+++ lib/bamboo/authenticate.py
@@ -1,6 +1,5 @@
 import cookielib
 import logging
-from publicsuffix import PublicSuffixList
 import re
 from .. import requests
 import urllib2
@@ -16,14 +15,11 @@ def external_authenticate(host, cookiefile, baseurl=''):
   retrieval_cookiejar.load(cookiefile, ignore_discard=True, ignore_expires=True)
 
   auth_cookies = []
-  psl = PublicSuffixList()
-  host_publicsuffix = psl.get_public_suffix(host)
   for c in retrieval_cookiejar:
-    if re.search(host, c.domain):
+    if re.search(c.domain, host):
       if re.search('JSESSIONID', c.name):
         logging.debug('%s %s %s', c.domain, c.name, c.value)
         auth_cookies.append(c)
-    elif re.search(host_publicsuffix, c.domain):
       if re.search('crowd.token_key', c.name):
         logging.debug('%s %s %s', c.domain, c.name, c.value)
         auth_cookies.append(c)
