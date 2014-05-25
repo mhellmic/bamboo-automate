72443cc5185c9a15fdefcadbb0948511a7a73eb4
Martin Hellmich <mhellmic@gmail.com>
2014-05-25 21:01:48 +0200

    add retry when a request fails; print avg req duration
    - retry MAXRETRIES times (5 by default) when a request fails
      for whatever reason. fail after some attempts
    - print the avg duration of the last 10 requests in the debug print

--- lib/requests.py
+++ lib/requests.py
@@ -7,6 +7,11 @@ import time
 import urllib
 import urllib2
 
+
+MAXRETRIES = 3
+RETRYSLEEP = 5
+
+
 class Connection:
   def __init__(self, hostname, baseurl, opener, auth_cookies):
     self.host = hostname
@@ -49,18 +54,37 @@ def post_ui_return_json(conn, path, params):
 def get_rest_return_json(conn, path, params):
   return get_ui_return_json(conn, path, params)
 
+def post_rest_return_json(conn, path, params):
+  return get_ui_return_json(conn, path, params)
+
+rolling_avg_counter = 1
+rolling_avg_list = []
+rolling_avg = 0
 def monitoring(func):
   @wraps(func)
   def with_time_monitoring(*args, **kwargs):
+    global rolling_avg_counter, rolling_avg_list, rolling_avg
     t1 = time.time()
     time.sleep(3.0)
     t2 = time.time()
-    logging.debug('slept %(sleep)s seconds.' % {'sleep': (t2-t1)})
+    logging.debug('slept %(sleep)s seconds.' % {'sleep': (t2 - t1)})
     start_time = time.time()
     res = func(*args, **kwargs)
     end_time = time.time()
-    logging.debug('%(fname)s took %(dur)s.' % {'fname':func.__name__,
-                                               'dur':(end_time - start_time)})
+    logging.debug('%(fname)s took %(dur)s.' % {'fname': func.__name__,
+                                               'dur': (end_time - start_time)})
+
+    rolling_avg_counter += 1
+    rolling_avg_list.append(end_time - start_time)
+    if rolling_avg_counter % 10 == 0:
+      rolling_avg_counter = 1
+      rolling_avg = ((rolling_avg + sum(rolling_avg_list)) /
+                     (1 + len(rolling_avg_list)))
+      rolling_avg_list = []
+      logging.debug('%(fname)s rolling average is %(dur)s.'
+                    % {'fname': func.__name__,
+                       'dur': rolling_avg})
+
     return res
   return with_time_monitoring
 
@@ -84,11 +108,22 @@ def _request(conn, method, path, params, headers, param_parse_func, response_par
   if len(cookies) > 0:
     req.add_header('Cookie', cookies)
 
-  logging.debug('%s', req.get_full_url())
-  if method == "POST":
-    response = conn.opener.open(req, param_parse_func(params))
-  elif method == "GET":
-    response = conn.opener.open(req)
+  retries = 0
+  req_success = False
+  while not req_success:
+    logging.debug('%s', req.get_full_url())
+    try:
+      if method == "POST":
+        response = conn.opener.open(req, param_parse_func(params))
+      elif method == "GET":
+        response = conn.opener.open(req)
+      req_success = True
+    except urllib2.URLError:
+      if retries >= MAXRETRIES:
+        raise
+      time.sleep(RETRYSLEEP)
+    finally:
+      retries += 1
 
   logging.debug('%s %s', response.geturl(), response.getcode())
   if response.getcode() > 399:
